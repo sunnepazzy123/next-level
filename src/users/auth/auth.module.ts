0a5d04aa0b46d2a6c9512c6ca7e4c72acebbde5f
sunnepazzy123 <sunday.odibo@onoco.com>
2023-05-28 19:35:14 +0200

    remove jwtModule from auth module

--- src/users/auth/auth.module.ts
+++ src/users/auth/auth.module.ts
@@ -2,23 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users.module';
 import { UsersService } from '../users.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService, UsersService],
-  imports: [
-    UsersModule,
-    JwtModule.registerAsync({
-      useFactory: (config: ConfigService) => {
-        return {
-          secret: config.get<string>('JWT_SECRET'),
-          signOptions: { expiresIn: '3d' },
-        };
-      },
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [UsersModule],
   exports: [AuthService],
 })
 export class AuthModule {}
